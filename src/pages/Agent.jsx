11b07fa867c28eb7d302e36d735960d9834fae34
OivaHuuskonen <156306030+OivaHuuskonen@users.noreply.github.com>
2025-10-11 22:21:29 +0300

    main_bg

--- src/pages/Agent.jsx
+++ src/pages/Agent.jsx
@@ -10,7 +10,6 @@ const PageHeader = ({ title }) => (
       <h1 className="font-Castoro text-align:left text-6xl md:text-6xl xl:text-7xl text-[#FBE9D0]">
         {title}
       </h1>
-      {/*<img src={artNouveau} alt="Art Nouveau" className="h-[6em] w-auto ml-4 bg-[#51829B]" />*/}
     </div>
   </div>
 );
@@ -65,19 +64,11 @@ export default function Agent() {
 
       <h2 className="text-center m-5"> recent Listings</h2>
 
-      {/*<div className="container">
-        <div className="row">
-          {ads?.map((ad) => (
-            <AdCard ad={ad} key={ad._id} />
-          ))}
-        </div>
-      </div>*/}
-
       <div className="grid grid-cols-1 
         sm:grid-cols-1 
         md:grid-cols-2 
         xl:grid-cols-3   
-        justify-center mb-10 gap-y-10 place-items-center px-4 sm:px-8 py-10 bg-[#FBE9D0]">
+        justify-center mb-10 gap-y-10 place-items-center px-4 sm:px-8 py-10 bg-[#FBE9D0] animate-fadeIn">
           {ads?.map((ad, index) => (
             <AdCard 
               ad={ad} 
