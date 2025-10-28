e7eed1ba04a02a8459b22a060d281b9268362ab3
OivaHuuskonen <156306030+OivaHuuskonen@users.noreply.github.com>
2025-10-28 19:37:37 +0200

    redirect

--- src/components/routes/RedirectRoute.jsx
+++ src/components/routes/RedirectRoute.jsx
@@ -1,24 +1,28 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Spinner from "../Spinner";
 
 export default function RedirectRoute() {
   const [count, setCount] = useState(3);
-
   const navigate = useNavigate();
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCount((currentCount) => --currentCount);
     }, 1000);
-    // redirect once count is equal to 0
+    
+    // Redirect once count is equal to 0
     count === 0 && navigate("/");
-    // cleanup
+    
+    // Cleanup
     return () => clearInterval(interval);
-  }, [count]);
+  }, [count, navigate]);
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <h3> {count} </h3>
+    <div className="flex items-center justify-center min-h-screen bg-[#FBE9D0]">
+      <div className="text-center">
+        <Spinner message={`Redirecting in ${count}...`} />
+      </div>
     </div>
   );
 }
