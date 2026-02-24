d453d717dab3f3932bb42853e96c146e8c5df7a0
OivaHuuskonen <156306030+OivaHuuskonen@users.noreply.github.com>
2026-02-24 14:46:52 +0200

    4256ret341

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.diea8rrqphg"
+    "revision": "0.8etfk4h3jc8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
